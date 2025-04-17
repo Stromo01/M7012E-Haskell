445865ba5062a269c6546fb77ab029275221e2f6
Frikkz <106763788+Frikkz@users.noreply.github.com>
2025-04-17 16:30:27 +0200

    statements

--- clean_parser_assignment/Parser.hs
+++ clean_parser_assignment/Parser.hs
@@ -1,24 +1,43 @@
-module Parser(module CoreParser, T, digit, digitVal, chars, letter, err,
-              lit, number, iter, accept, require, token,
-              spaces, word, (-#), (#-)) where
-import Prelude hiding (return, fail,iterate)
-import Data.Char
+module Parser
+  ( module CoreParser,
+    T,
+    digit,
+    digitVal,
+    chars,
+    letter,
+    err,
+    lit,
+    number,
+    iter,
+    accept,
+    require,
+    token,
+    spaces,
+    word,
+    (-#),
+    (#-),
+  )
+where
+
 import CoreParser
+import Data.Char
+import Prelude hiding (fail, iterate, return)
+
 infixl 7 -#, #-
 
 type T a = Parser a
 
 err :: String -> Parser a
-err message cs = error (message++" near "++cs++"\n")
+err message cs = error (message ++ " near " ++ cs ++ "\n")
 
 iter :: Parser a -> Parser [a]
-iter m = m # iter m >-> cons ! return []
+iter m = m # iter m >-> uncurry (:) ! return []
 
 iterate :: Parser a -> Int -> Parser [a]
 iterate m 0 = return []
-iterate m i = m # iterate m (i-1) >-> uncurry(:)
+iterate m i = m # iterate m (i - 1) >-> uncurry (:)
 
-cons(a, b) = a:b
+cons (a, b) = a : b
 
 (-#) :: Parser a -> Parser b -> Parser b -- Accepts b and ignores a
 m -# n = (m # n) >-> snd
@@ -42,13 +61,13 @@ chars :: Int -> Parser String -- Accepts a string of length n
 chars n = token (iterate (char ? isAlpha) n)
 
 accept :: String -> Parser String
-accept w = token (iterate char (length w)) ? (==w)
+accept w = token (iterate char (length w)) ? (== w)
 
 require :: String -> Parser String -- accepts w and reports the error
 require w = accept w ! err ("Expected: " ++ w)
 
 lit :: Char -> Parser Char
-lit c = token char ? (==c)
+lit c = token char ? (== c)
 
 digit :: Parser Char
 digit = char ? isDigit
@@ -57,8 +76,10 @@ digitVal :: Parser Integer
 digitVal = digit >-> digitToInt >-> fromIntegral
 
 number' :: Integer -> Parser Integer
-number' n = digitVal #> (\ d -> number' (10*n+d))
-          ! return n
+number' n =
+  digitVal
+    #> (\d -> number' (10 * n + d))
+    ! return n
+
 number :: Parser Integer
 number = token (digitVal #> number')
-
--- clean_parser_assignment/Statement.hs
+++ clean_parser_assignment/Statement.hs
@@ -1,51 +1,64 @@
-module Statement(T, parse, toString, fromString, exec) where
-import Prelude hiding (return, fail)
-import Parser hiding (T)
-import qualified Dictionary
-import qualified Expr
-import Data.Binary.Get (skip)
-import Distribution.Simple.Setup (BuildFlags(BuildFlags))
-type T = Statement
-data Statement =
-    Assignment String Expr.T |  -- variable ':=' expr ';'
-    Skip |                      -- 'skip' ';'
-    Begin [Statement] |         -- 'begin' statements 'end'
-    If Expr.T Statement Statement |  -- 'if' expr 'then' statement 'else' statement
-    While Expr.T Statement |    -- 'while' expr 'do' statement
-    Read String |               -- 'read' variable ';'
-    Write Expr.T                -- 'write' expr ';'
-    deriving Show
+module Statement (T, parse, toString, fromString, exec) where
 
-newtype Statements = Statements [Statement] deriving Show
+import Data.Binary.Get (skip)
+import Dictionary qualified
+import Distribution.Simple.Setup (BuildFlags (BuildFlags))
+import Expr qualified
+import Parser hiding (T)
+import Prelude hiding (fail, return)
+
+type T = Statement
+
+data Statement
+  = Assignment String Expr.T -- variable ':=' expr ';'
+  | Skip -- 'skip' ';'
+  | Begin [Statement] -- 'begin' statements 'end'
+  | If Expr.T Statement Statement -- 'if' expr 'then' statement 'else' statement
+  | While Expr.T Statement -- 'while' expr 'do' statement
+  | Read String -- 'read' variable ';'
+  | Write Expr.T -- 'write' expr ';'
+  deriving (Show)
+
+newtype Statements = Statements [Statement] deriving (Show)
+
 assignment :: Parser Statement
 assignment = word #- accept ":=" # Expr.parse #- require ";" >-> buildAss
+
 buildAss (v, e) = Assignment v e
 
+skipStmt :: Parser Statement
 skipStmt = accept "skip" -# require ";" >-> const Skip
+
+beginStmt :: Parser Statement
 beginStmt = accept "begin" -# iter parse #- require "end" >-> Begin
 
+ifStmt :: Parser Statement
 ifStmt = (accept "if" -# Expr.parse #- require "then" # parse #- require "else" # parse) >-> buildIf
+
+buildIf :: ((Expr.T, Statement), Statement) -> Statement
 buildIf ((cond, thenStmts), elseStmts) = If cond thenStmts elseStmts
 
+whileStmt :: Parser Statement
 whileStmt = (accept "while" -# Expr.parse #- require "do" # parse) >-> buildWhile
+
+buildWhile :: (Expr.T, Statement) -> Statement
 buildWhile (cond, stmt) = While cond stmt
 
+readStmt :: Parser Statement
 readStmt = accept "read" -# word #- require ";" >-> Read
+
+writeStmt :: Parser Statement
 writeStmt = accept "write" -# Expr.parse #- require ";" >-> Write
 
 exec :: [T] -> Dictionary.T String Integer -> [Integer] -> [Integer]
-exec (If cond thenStmts elseStmts: stmts) dict input =
-    if (Expr.value cond dict)>0
-    then exec (thenStmts: stmts) dict input
-    else exec (elseStmts: stmts) dict input
+exec (If cond thenStmts elseStmts : stmts) dict input =
+  if (Expr.value cond dict) > 0
+    then exec (thenStmts : stmts) dict input
+    else exec (elseStmts : stmts) dict input
+
+parseStatement :: Parser Statement
+parseStatement = assignment ! skipStmt ! beginStmt ! ifStmt ! whileStmt ! readStmt ! writeStmt
 
 instance Parse Statement where
-  parse = assignment !
-          skipStmt !
-          beginStmt !
-          ifStmt !
-          whileStmt !
-          readStmt !
-          writeStmt !
-          err "illegal statement"
+  parse = parseStatement
   toString = error "Statement.toString not implemented"
